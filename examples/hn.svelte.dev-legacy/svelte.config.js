d900679bc67ca967067f3825860f1f83d063f6b0
dishuostec <dishuostec@gmail.com>
2021-11-26 11:22:44 +0800

    build legacy

--- examples/hn.svelte.dev-legacy/svelte.config.js
+++ examples/hn.svelte.dev-legacy/svelte.config.js
@@ -1,8 +1,45 @@
-import netlify from '@sveltejs/adapter-netlify';
+import node from '@sveltejs/adapter-node';
+import { additionalLegacyPolyfills, legacy as ie11 } from '@dishuostec/sveltekit-legacy-ie11';
 
-export default {
+let legacy;
+const LEGACY = process.env.LEGACY;
+
+switch (LEGACY) {
+	case 'no-ie':
+		legacy = {
+			targets: ['defaults', 'not ie > 0']
+		};
+		break;
+	case 'defaults':
+		legacy = {
+			targets: ['defaults'],
+			additionalLegacyPolyfills,
+			plugins: {
+				legacy: ie11
+			}
+		};
+		break;
+}
+
+const out = `build${LEGACY ? `-${LEGACY}` : ''}`;
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	kit: {
-		adapter: netlify(),
-		target: '#svelte'
+		legacy,
+		adapter: node({
+			out
+		}),
+		target: '#svelte',
+		vite: {
+			build: {
+				minify: false, // minify will force make some tests fail.
+				rollupOptions: {
+					treeshake: false // treeshake will force make some tests fail.
+				}
+			}
+		}
 	}
 };
+
+export default config;
